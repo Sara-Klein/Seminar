f731fa7e1a230fe812057e64f9487b5e8f10dc22
Sara Klein <sara_klein@gmx.de>
2022-04-25 14:09:15 +0200

    Update multi_level.py

--- multi_level.py
+++ multi_level.py
@@ -23,13 +23,22 @@ class Multilevel():
         self.eps_0 = 1
         self.rho = (1/2) * (1+R)
         self.gamma_0 = (1+R)/L
+       
+    '''
+        self.network = PolicyNet(a,b) für richtige dimensionen a,b
+        theta_0 braucht man nicht
+    '''
+        
+        
 
     # Not needed in more complex model
     def grad_soft(self, theta):
 
         return np.eye(len(theta),len(theta))- np.matmul(softmax(theta).reshape(len(theta),1), np.ones((1,len(theta))))
 
-    def sample_env(self, theta, t, state):
+    def sample_env(self, theta, t, state): 
+        ########################## input action fehlt ############ self.network übergeben - mit netzwerk arbeiten statt mit theta!
+
         # TODO: Sollten wir in der Theorie nicht samplen bis das Spiel immer vorbei ist? Es solllte jetzt schneller gehen
         # Ich kann auch versuchen Offline Reinforcement Learning zu etablieren
         rewards = 0
@@ -75,9 +84,17 @@ class Multilevel():
     #Das ist unser Code
     def multi_level_step(self):
         # self.theta = self.theta_0.copy()
-        self.theta = self.theta_0.clone().detach()
-        print(self.theta)
+        self.theta = self.theta_0.clone().detach()  ########## braucht man nicht?
+        print(self.theta) 
+        '''
+            mit optimzer arbeiten:
+            optimizer = optim.SGD(self.network.parameters())
+            optimizer.param_groups[0]['lr'] = 0.001 (oder was auch immer)
+        '''
         for n in range(1,5): # 5 frei gewählt, um schnelle Resultate zu sehen
+
+             
+           
 
             eps_n = self.eps_0 * 1/(n**self.rho)
             sigma_n = 1/(n**R) * self.sigma_0
@@ -105,7 +122,7 @@ class Multilevel():
                         acts.append(action)
                         rew_to_go = 0
                         for u in range(M**k):
-                            _, rew_to_go = self.sample_env(self.theta, state=state, action=action)
+                            _, rew_to_go = self.sample_env(self.theta, state=state, action=action) #### self.network übergeben - mit netzwerk arbeiten statt mit theta
                             # t_to_go = self.sample_env(self.theta, t, state=state, action=action)
                             # rew = np.array(t_to_go).reshape(int((len(t_to_go))/2),2)
                             # for tup in rew[0:]:
@@ -116,7 +133,7 @@ class Multilevel():
                         else: 
                             rew_to_go2 = 0
                             for u in range(M**(k-1)):
-                                _,rew_to_go2 = self.sample_env(self.theta, t, state=state, action=action)
+                                _,rew_to_go2 = self.sample_env(self.theta, t, state=state, action=action) #### self.network übergeben - mit netzwerk arbeiten statt mit theta
                                 # rew = np.array(t_to_go).reshape(int((len(t_to_go))/2),2)
                                 # for tup in rew[0:]:
                                 #     # think about how to model the reward with the environment
@@ -125,15 +142,29 @@ class Multilevel():
                             Q_k2.append(1/(M**(k-1)) * rew_to_go2)
                     total_acts.append(acts)  
 
+                    '''
+                    mit optimzer arbeiten:
+                    optimizer.zero_grad()
+                    with torch.set_grad_enabled(True):
+                        loss = ? ..... hier alles was mit dem netzwerk zu tun hat in der Berechnung und was abgeleitet werden soll!, der rest kann vorher berechnet werden!
+                        loss.backward()
+                        optimizer.step()
+                    scheduler.step()
+                    '''
+
                     # TODO: Network should be depended of state, need to check if it works correctly, idea: do forward pass to get actions, 
                     L_k = L_k +  np.sum(np.multiply(PolicyNet(self.env.observation_space.shape, self.env.action_space.n).backward(gradient=self.theta)[acts],(np.array(Q_k)-np.array(Q_k2))[:,np.newaxis]), axis=0)
                     # L_k = L_k +  np.sum(np.multiply(self.grad_soft(self.theta)[acts],(np.array(Q_k)-np.array(Q_k2))[:,np.newaxis]), axis=0)
+                    
                 L_m = L_m +((1/N_nk) * L_k)
             plt.hist(np.array(total_acts).reshape(np.product(np.shape(total_acts))))
             plt.show()
             print(np.sum(L_m))
             self.theta = self.theta +  torch.from_numpy(gamma_n * L_m)
         return self.theta
+
+
+       
 
 
 if __name__ == '__main__':
